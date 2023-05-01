fa562c258e27170958865988fef7b93b9f41f7ea
Bipin <bskori9175@gmail.com>
2023-05-01 11:13:31 +0530

    SiteAdmin Medium controller added

---
+++ FutureStage/Data/Services/SiteAdminServices/MediumService.cs
@@ -0,0 +1,14 @@
+﻿using FutureStage.Data.Base;
+using FutureStage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FutureStage.Data.Services.SiteAdminServices
+{
+    public class MediumService : EntityBaseRepository<Medium>, IMediumService 
+    {
+        public MediumService(AppDbContext context) : base(context) { }
+    }
+}
--- FutureStage/Models/Medium.cs
+++ FutureStage/Models/Medium.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FutureStage.Data.Base;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 namespace FutureStage.Models
 {
     [Table("MediumTbl")]
-    public class Medium
+    public class Medium : IEntityBase
     {
         [Key]
         public int ID { get; set; }
