263e6040ff2adc9492968efc676492f61f2dc2ee
Bipin <bskori9175@gmail.com>
2023-05-16 11:43:18 +0530

    Schools doLogin ui page added

---
+++ FutureStage/Controllers/ManageSchoolsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FutureStage.Controllers
+{
+    public class ManageSchoolsController : Controller
+    {
+
+        public IActionResult doLogin()
+        {
+            return View();
+        }
+    }
+}
