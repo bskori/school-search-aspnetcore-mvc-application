1df6d0d7b679fa9753e0dddea1c643c1cdd20955
Bipin <bskori9175@gmail.com>
2023-05-27 23:47:04 +0530

    Schools SchoolFacility controller added

--- FutureStage/Models/SchoolFacility.cs
+++ FutureStage/Models/SchoolFacility.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FutureStage.Data.Base;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 namespace FutureStage.Models
 {
     [Table("SchoolFacilityTbl")]
-    public class SchoolFacility
+    public class SchoolFacility : IEntityBase
     {
         [Key]
         public int ID { get; set; }
