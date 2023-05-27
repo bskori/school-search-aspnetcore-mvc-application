f416b491e5896be7f257ff5cd077da0f81de6687
Bipin <bskori9175@gmail.com>
2023-05-27 23:59:04 +0530

    Schools SchoolAchivement service added

--- FutureStage/Models/SchoolAchivement.cs
+++ FutureStage/Models/SchoolAchivement.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FutureStage.Data.Base;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 namespace FutureStage.Models
 {
     [Table("SchoolAchivementTbl")]
-    public class SchoolAchivement
+    public class SchoolAchivement : IEntityBase
     {
         [Key]
         public int ID { get; set; }
