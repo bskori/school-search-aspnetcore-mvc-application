55cd7fcc3c241892f25bfa18009d0635527b0bd1
Bipin <bskori9175@gmail.com>
2023-05-28 23:45:45 +0530

    Schools IAdmissionPrerequisite and IAdmissionProcessService added

--- FutureStage/Models/AdmissionProcess.cs
+++ FutureStage/Models/AdmissionProcess.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FutureStage.Data.Base;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 namespace FutureStage.Models
 {
     [Table("AdmissionProcessTbl")]
-    public class AdmissionProcess
+    public class AdmissionProcess : IEntityBase
     {
         [Key]
         public int ID { get; set; }
