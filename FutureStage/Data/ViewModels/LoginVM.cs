2f3d305c565eb156e10f29edb6ddf169df1c087a
Bipin <bskori9175@gmail.com>
2023-06-28 17:41:33 +0530

    Frontend login page design enhanced

--- FutureStage/Data/ViewModels/LoginVM.cs
+++ FutureStage/Data/ViewModels/LoginVM.cs
@@ -10,7 +10,7 @@ namespace FutureStage.Data.ViewModels
     {
         [Display(Name ="Email address")]
         [Required(ErrorMessage ="Please enter a email address.")]
-        [EmailAddress(ErrorMessage ="Invalid email address type.")]
+        [EmailAddress(ErrorMessage ="Please enter a valid email address.")]
         public string EmailAddress { get; set; }
 
         [DataType(DataType.Password)]
