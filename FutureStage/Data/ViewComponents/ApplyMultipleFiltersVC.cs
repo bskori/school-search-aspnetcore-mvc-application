3098fd9f05c0ed08a0da62669b389419a9afd434
Bipin <bskori9175@gmail.com>
2023-07-07 00:38:33 +0530

    multiple filters cards functionality added

--- FutureStage/Data/ViewComponents/ApplyMultipleFiltersVC.cs
+++ FutureStage/Data/ViewComponents/ApplyMultipleFiltersVC.cs
@@ -8,9 +8,18 @@ namespace FutureStage.Data.ViewComponents
 {
     public class ApplyMultipleFiltersVC : ViewComponent
     {
-
+        private readonly AppDbContext _context;
+        public ApplyMultipleFiltersVC(AppDbContext context)
+        {
+            _context = context;
+        }
         public IViewComponentResult Invoke()
         {
+            ViewBag.Boards = _context.EducationBoards.ToList();
+            ViewBag.Cities = _context.Cities.ToList();
+            ViewBag.Areas = _context.Areas.ToList();
+            ViewBag.States = _context.States.ToList();
+            ViewBag.Standards = _context.Standards.ToList();
             return View();
         }
 
