7bde37d62ecab7ddba78e41eb1c04daf3a696d5f
Bipin <bskori9175@gmail.com>
2023-07-06 23:12:50 +0530

    schools card slider feature added

--- FutureStage/wwwroot/FrontendAssets/js/script.js
+++ FutureStage/wwwroot/FrontendAssets/js/script.js
@@ -1,74 +1,46 @@
+////$(document).ready(function () {
+////    var sliderWrapper = $('.slider-wrapper');
+////    var sliderItems = $('.slider-item');
+////    var slideWidth = sliderItems.first().outerWidth();
+////    var currentIndex = 0;
 
-$(document).ready(function () {
-    var sliderWrapper = $('.slider-wrapper');
-    var sliderItems = $('.slider-item');
-    var totalItems = sliderItems.length;
-    var slideInterval;
-    var currentIndex = 0;
-    var slideWidth = sliderItems.first().outerWidth();
+////    function cloneCards() {
+////        var clonedItems = sliderItems.clone();
+////        sliderWrapper.append(clonedItems);
+////    }
 
-  
-    var clonedItems = sliderItems.clone();
-    sliderWrapper.append(clonedItems);
+////    function slideToCurrent() {
+////        var translateX = -currentIndex * slideWidth;
+////        sliderWrapper.css('transition', 'transform 0.6s ease');
+////        sliderWrapper.css('transform', 'translateX(' + translateX + 'px)');
+////    }
 
-    $('.next-btn').click(function () {
-        nextSlide();
-    });
+////    function nextSlide() {
+////        currentIndex++;
+////        slideToCurrent();
+////    }
 
-    $('.prev-btn').click(function () {
-        prevSlide();
-    });
+////    function prevSlide() {
+////        currentIndex--;
+////        slideToCurrent();
+////    }
 
-    function nextSlide() {
-        currentIndex++;
-        slideToCurrent();
-    }
+////    function startAutoSlide() {
+////        setInterval(function () {
+////            cloneCards();
+////            nextSlide();
+////        }, 3000);
+////    }
 
-    function prevSlide() {
-        currentIndex--;
-        slideToCurrent();
-    }
+////    $('.slider-next-btn').click(function () {
+////        cloneCards();
+////        nextSlide();
+////    });
 
-    function slideToCurrent() {
-        var translateX = -currentIndex * slideWidth;
-        sliderWrapper.css('transform', 'translateX(' + translateX + 'px)');
+////    $('.slider-prev-btn').click(function () {
+////        cloneCards();
+////        prevSlide();
+////    });
 
-        if (currentIndex === totalItems * 2) {
-            currentIndex = 0;
-            setTimeout(function () {
-                sliderWrapper.css('transition', 'none');
-                sliderWrapper.css('transform', 'translateX(0)');
-                setTimeout(function () {
-                    sliderWrapper.css('transition', '');
-                }, 10);
-            }, 600);
-        } else if (currentIndex < 0) {
-            currentIndex = totalItems * 2 - 1;
-            setTimeout(function () {
-                var translateX = -currentIndex * slideWidth;
-                sliderWrapper.css('transition', 'none');
-                sliderWrapper.css('transform', 'translateX(' + translateX + 'px)');
-                setTimeout(function () {
-                    sliderWrapper.css('transition', '');
-                }, 10);
-            }, 600);
-        }
-    }
-
-    function startAutoSlide() {
-        slideInterval = setInterval(nextSlide, 3000);
-    }
-
-    function stopAutoSlide() {
-        clearInterval(slideInterval);
-    }
-
-    sliderWrapper.hover(function () {
-        stopAutoSlide();
-    }, function () {
-        startAutoSlide();
-    });
-
-    startAutoSlide();
-});
-
+////    startAutoSlide();
+////});
