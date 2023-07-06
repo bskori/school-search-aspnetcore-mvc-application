b7a823148f197297ea4b0c98bf2533c287fe5827
Bipin <bskori9175@gmail.com>
2023-07-06 23:57:49 +0530

    commiting before branch change

---
+++ FutureStage/Data/ViewComponents/ApplyMultipleFiltersVC.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FutureStage.Data.ViewComponents
+{
+    public class ApplyMultipleFiltersVC : ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
