c35c8e8920bd00f478c66a3ebbda30cfdfc3f3a2
Bipin <bskori9175@gmail.com>
2023-07-06 23:13:14 +0530

    schools card slider feature added

---
+++ FutureStage/Data/ViewComponents/PopularSchoolsVC.cs
@@ -0,0 +1,25 @@
+﻿using FutureStage.Data.Services.SchoolsServices;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FutureStage.Data.ViewComponents
+{
+    public class PopularSchoolsVC : ViewComponent
+    {
+        private readonly ISchoolService _schoolService;
+        private readonly AppDbContext _context;
+        public PopularSchoolsVC(AppDbContext context ,ISchoolService schoolService)
+        {
+            _schoolService = schoolService;
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(_context.Schools.ToList());
+        }
+    }
+}
