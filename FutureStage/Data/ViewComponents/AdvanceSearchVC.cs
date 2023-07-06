4f731dd15e420cd58535f7c636c9ba7f6682ad80
Bipin <bskori9175@gmail.com>
2023-07-06 16:41:48 +0530

    Advance searchbar code added agian

--- FutureStage/Data/ViewComponents/AdvanceSearchVC.cs
+++ FutureStage/Data/ViewComponents/AdvanceSearchVC.cs
@@ -1,11 +1,27 @@
-﻿using System;
+﻿using FutureStage.Data.Services.SiteAdminServices;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace FutureStage.Data.ViewComponents
 {
-    public class AdvanceSearchVC
+    public class AdvanceSearchVC : ViewComponent
     {
+        private readonly AppDbContext _context;
+        public AdvanceSearchVC(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.States = _context.States.ToList();
+            ViewBag.Cities = _context.Cities.ToList();
+            ViewBag.Areas = _context.Areas.ToList();
+         
+            return View();
+        }
     }
 }
