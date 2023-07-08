02c3b92214462bc3b59e26e747ed9acc2a2f1ef1
Bipin <bskori9175@gmail.com>
2023-07-09 00:03:58 +0530

    school details page design enhanced

--- FutureStage/wwwroot/FrontendAssets/js/script.js
+++ FutureStage/wwwroot/FrontendAssets/js/script.js
@@ -1,108 +1,79 @@
-////$(document).ready(function () {
-////    var sliderwrapper = $('.slider-wrapper');
-////    var slideritems = $('.slider-item');
-////    var slidewidth = slideritems.first().outerwidth();
-////    var currentindex = 0;
 
-////    function clonecards() {
-////        var cloneditems = slideritems.clone();
-////        sliderwrapper.append(cloneditems);
-////    }
-
-////    function slidetocurrent() {
-////        var translatex = -currentindex * slidewidth;
-////        sliderwrapper.css('transition', 'transform 0.6s ease');
-////        sliderwrapper.css('transform', 'translatex(' + translatex + 'px)');
-////    }
-
-////    function nextslide() {
-////        currentindex++;
-////        slidetocurrent();
-////    }
-
-////    function prevslide() {
-////        currentindex--;
-////        slidetocurrent();
-////    }
-
-////    function startautoslide() {
-////        setinterval(function () {
-////            clonecards();
-////            nextslide();
-////        }, 3000);
-////    }
-
-////    $('.slider-next-btn').click(function () {
-////        clonecards();
-////        nextslide();
-////    });
-
-////    $('.slider-prev-btn').click(function () {
-////        clonecards();
-////        prevslide();
-////    });
-
-////    startAutoSlide();
-////});
 $(document).ready(function () {
-    var sliderWrapper = $('.slider-wrapper');
-    var sliderItems = $('.slider-item');
-    var slideWidth = sliderItems.first().outerWidth();
-    var totalItems = sliderItems.length;
-    var currentIndex = 0;
+    var sliderwrapper = $('.slider-wrapper');
+    var slideritems = $('.slider-item');
+    var slidewidth = slideritems.first().outerWidth();
+    var currentindex = 0;
+    var totalitems = slideritems.length;
 
-    function slideToCurrent() {
-        var translateX = -currentIndex * slideWidth;
-        sliderWrapper.css('transition', 'transform 0.6s ease');
-        sliderWrapper.css('transform', 'translateX(' + translateX + 'px)');
+    function clonecards() {
+        var cloneditems = slideritems.clone();
+        sliderwrapper.append(cloneditems);
     }
 
-    function nextSlide() {
-        currentIndex++;
-        if (currentIndex >= totalItems) {
-            currentIndex = 0;
-            sliderWrapper.css('transition', 'none');
-            sliderWrapper.css('transform', 'translateX(0)');
+    function slidetocurrent() {
+        var translatex = -currentindex * slidewidth;
+        sliderwrapper.css('transition', 'transform 0.6s ease');
+        sliderwrapper.css('transform', 'translateX(' + translatex + 'px)');
+    }
+
+    function nextslide() {
+        currentindex++;
+        if (currentindex >= totalitems) {
+            
+            currentindex = 0;
+            
+            sliderwrapper.css('transition', 'none');
+            sliderwrapper.css('transform', 'translateX(0)');
+            
             setTimeout(function () {
-                sliderWrapper.css('transition', 'transform 0.6s ease');
-                slideToCurrent();
-            }, 10);
+                
+                sliderwrapper.css('transition', 'transform 0.6s ease');
+                
+                slidetocurrent();
+            }, 0);
         } else {
-            slideToCurrent();
+            slidetocurrent();
         }
     }
 
-    function prevSlide() {
-        currentIndex--;
-        if (currentIndex < 0) {
-            currentIndex = totalItems - 1;
-            sliderWrapper.css('transition', 'none');
-            sliderWrapper.css('transform', 'translateX(' + (-slideWidth * totalItems) + 'px)');
+    function prevslide() {
+        currentindex--;
+        if (currentindex < 0) {
+            
+            currentindex = totalitems - 1;
+            
+            var translatex = -totalitems * slidewidth;
+            sliderwrapper.css('transition', 'none');
+            sliderwrapper.css('transform', 'translateX(' + translatex + 'px)');
+           
             setTimeout(function () {
-                sliderWrapper.css('transition', 'transform 0.6s ease');
-                slideToCurrent();
-            }, 10);
+              
+                sliderwrapper.css('transition', 'transform 0.6s ease');
+                
+                slidetocurrent();
+            }, 0);
         } else {
-            slideToCurrent();
+            slidetocurrent();
         }
     }
 
-    function startAutoSlide() {
+    function startautoslide() {
         setInterval(function () {
-            nextSlide();
+            clonecards();
+            nextslide();
         }, 3000);
     }
 
     $('.slider-next-btn').click(function () {
-        nextSlide();
+        clonecards();
+        nextslide();
     });
 
     $('.slider-prev-btn').click(function () {
-        prevSlide();
+        clonecards();
+        prevslide();
     });
 
-    startAutoSlide();
+    startautoslide();
 });
-
-
-
